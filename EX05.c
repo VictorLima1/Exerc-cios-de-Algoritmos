a55f3a845185a980526c856a595ccd76dc51e49f
Victor <victormanoel119@gmail.com>
2018-06-10 17:46:21 -0300

    Probleminhas na hora de deletar o aluno

--- EX05.c
+++ EX05.c
@@ -14,15 +14,16 @@ typedef struct{
 Aluno* leArquivo(Aluno *a, int n) {
   FILE* f = fopen("registros.txt","r");
   int i;
-  for (i = 0; i < n; i++) {
-  	fscanf(f,"%s", (a[i].registro));
-  	fscanf(f,"%s", (a[i].nome));
-  	fscanf(f,"%s", (a[i].email));
-  	fscanf(f,"%s", (a[i].telefone));
-  	fscanf(f,"%i", (a[i].p1));
-  	fscanf(f,"%i", (a[i].p2));
-  	fscanf(f,"%i", (a[i].p3));
-  	fscanf(f,"%i", (a[i].media));
+  for (i = 0; i < n; i = i+1) {
+  	fscanf(f,"%s ", a[i].registro);
+    fgets(a[i].nome, 50, f);
+		a[i].registro[strcspn(a[i].registro, "\n")] = 0;
+  	fscanf(f,"%s", a[i].email);
+  	fscanf(f,"%s", a[i].telefone);
+  	fscanf(f,"%i", &a[i].p1);
+  	fscanf(f,"%i", &a[i].p2);
+  	fscanf(f,"%i", &a[i].p3);
+  	fscanf(f,"%i", &a[i].media);
   }
   fclose(f);
 }
@@ -42,73 +43,65 @@ int contaLinhas(char* a){
 
 void insereAluno(){
 	Aluno a;
-	fflush(stdin);
 	printf("Registro do aluno: ");
 	fflush(stdin);
-	scanf("%s", a.registro);
-	fflush(stdin);
+	fgets(a.registro, 10, stdin);
+	a.registro[strcspn(a.registro, "\n")] = 0;
 	printf("\nNome do aluno: ");
 	fflush(stdin);
-	scanf("%s", a.nome);
-	fflush(stdin);
+	fgets(a.nome, 50, stdin);
+	a.nome[strcspn(a.nome, "\n")] = 0;
 	printf("\nE-mail do aluno: ");
-	fflush(stdin);
-	scanf("%s", a.email);
-	fflush(stdin);
+	fgets(a.email, 30, stdin);
+	a.email[strcspn(a.email, "\n")] = 0;
 	printf("\nTelefone do aluno: ");
-	fflush(stdin);
-	scanf("%s", a.telefone);
-	fflush(stdin);
+	fgets(a.telefone, 20, stdin);
+	a.telefone[strcspn(a.telefone, "\n")] = 0;
 	printf("\nNota da P1: ");
-	fflush(stdin);
 	scanf("%i", &a.p1);
-	fflush(stdin);
 	printf("\nNota da P2: ");
-	fflush(stdin);
 	scanf("%i", &a.p2);
-	fflush(stdin);
 	printf("\nNota da P3: ");
-	fflush(stdin);
 	scanf("%i", &a.p3);
 
 	a.media = (a.p1 + a.p2 + a.p3)/3;
 	FILE* f = fopen("registros.txt", "a");
-	fprintf(f, "%s %s %s %s %i %i %i %i\n", a.registro, a.nome, a.email, a.telefone, a.p1, a.p2, a.p3, a.media);
+	fprintf(f, "%s\n%s\n%s\n%s\n%i\n%i\n%i\n%i\n", a.registro, a.nome, a.email, a.telefone, a.p1, a.p2, a.p3, a.media);
 	fclose(f);
 }
 
 void mostraAluno(Aluno *a, int n){
 	int i = 0;
   	for (i = 0; i < n; i++) {
-		if(a[i].nome == ""){
+			//fscanf()
+		if(a[i].p1 == 0){
 
 		}
 		else{
-			printf("Registro: %s\nNome: %s\nE-mail: %s\nTelefone: %s\nP1: %i\nP2: %i\nP3: %i\nMédia: %i\n\n", a[i].registro, a[i].nome, a[i].email, a[i].telefone, a[i].p1, a[i].p2, a[i].p3, a[i].media);
+			printf("Registro: %s\nNome: %sE-mail: %s\nTelefone: %s\nP1: %i\nP2: %i\nP3: %i\nMedia: %i\n\n", a[i].registro, a[i].nome, a[i].email, a[i].telefone, a[i].p1, a[i].p2, a[i].p3, a[i].media);
 		}
   	}
 	puts("");
 }
 
 void deletaAluno(Aluno *a, int n){
-	system("clear");
+	//system("clear");
 	FILE* f = fopen("registros.txt", "w");
 	int opc, i = 0;
 	printf("Quer apagar apenas um ou todos os alunos?\n1 - Apenas um\n2 - Todos\n\n");
 	scanf("%i", &opc);
 
 	if(opc == 1){
-		system("clear");
-		char nome[50];
-		printf("Qual o nome do aluno que deseja apagar? ");
-		scanf("%s", nome);
+		//system("clear");
+		char matricula[50];
+		printf("Qual o regisstro do aluno que deseja apagar? ");
+		scanf("%s", matricula);
 		for (i = 0; i < n; i++){
-			if(strcmp(nome, a[i].nome) != 0){
-				fprintf(f, "%s %s %s %s %i %i %i %i\n", a[i].registro, a[i].nome, a[i].email, a[i].telefone, a[i].p1, a[i].p2, a[i].p3, a[i].media);
+			if(matricula != a[i].registro){
+				fprintf(f, "%s\n%s\n%s\n%s\n%i\n%i\n%i\n%i\n", a[i].registro, a[i].nome, a[i].email, a[i].telefone, a[i].p1, a[i].p2, a[i].p3, a[i].media);
 			}
 		}
-		system("clear");
-		printf("%s deletado(a).", nome);
+		//system("clear");
 		puts("");
 		puts("");
 	}
@@ -123,7 +116,7 @@ int main(){
 		printf("1 - Inserir aluno\n2 - Mostrar todos\n3 - Alterar dados\n4 - Deletar aluno\n0 - Sair\n");
 		scanf("%d", &opc2);
 
-		system("clear");
+		//system("clear");
 
 		if(opc2 == 1){
 			insereAluno();
